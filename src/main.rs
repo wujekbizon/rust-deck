059fa23b33d1c70c2f906e93720b5034cd4c88ac
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2024-09-08 08:20:15 +0200

    Morning Rust practice

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,48 @@
-#[derive(Debug)]
+use rand::{seq::SliceRandom, thread_rng};
+use std::slice;
 
+#[derive(Debug)]
 struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    let deck: Deck = Deck { cards: Vec::new() };
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+        let values = [
+            "Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four",
+            "Three", "Two",
+        ];
 
-    println!("Heres your deck: {:?}", deck);
+        let mut cards = vec![];
+
+        for suit in suits {
+            for value in values {
+                let card = format!("{value} of {suit}");
+                cards.push(card);
+            }
+        }
+
+        Deck { cards }
+    }
+
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
+        self.cards.shuffle(&mut rng);
+    }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+    }
+}
+
+fn main() {
+    let mut deck = Deck::new();
+    deck.shuffle();
+
+    // Need t oadd error handling!
+    let hand = deck.deal(6);
+
+    println!("Heres your hand: {:#?}", hand);
+    println!("Heres your deck: {:#?}", deck);
 }
